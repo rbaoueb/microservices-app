471c3a4f8ac046c249c09bb1f960af73480023a1
ridha <ridha.baoueb@bns.tn>
2025-09-25 22:44:50 +0200

    Refactor CustomerController and enhance request handling
    Added logging for key endpoints, improved request validation with `@Valid`, and refined response handling using `ServletUriComponentsBuilder`. Migrated to `@RequiredArgsConstructor` for dependency injection and updated media type specifications to ensure consistent API behavior.

--- customer-service/src/main/java/com/example/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/example/customerservice/CustomerServiceApplication.java
@@ -3,9 +3,15 @@ package com.example.customerservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                io.camunda.zeebe.spring.client.configuration.ZeebeClientProdAutoConfiguration.class
+        }
+)
 @EnableDiscoveryClient
+@EnableScheduling
 public class CustomerServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
